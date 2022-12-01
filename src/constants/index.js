13a15ff716fd747fa3b58953f48ed96823f7da38
kayabegum123 <91554169+kayabegum123@users.noreply.github.com>
2022-11-30 22:56:18 -0500

    final

---
+++ src/constants/index.js
@@ -0,0 +1,28 @@
+const COLORS = {
+  primary: "#7a0c96",
+  secondary: "#E9E9E9",
+  accent: "#46254f",
+  textColor: "#461453",
+};
+
+const SIZES = {
+  padding: 19,
+  borderRadius: 15,
+  textBoxRadius: 25,
+  h1: 24,
+  h2: 20,
+};
+
+const FONTS = {
+  h1_semiBold: { fontSize: SIZES.h1 },
+  h2_semiBold: { fontSize: SIZES.h2 },
+};
+
+const SHADOW = {
+  elevation: 5,
+  shadowColor: COLORS.secondary,
+  shadowOffset: { width: 2, height: 12 },
+  shadowRadius: 12,
+};
+
+export { COLORS, SIZES, FONTS, SHADOW };
